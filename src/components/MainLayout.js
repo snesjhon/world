2cdc3d606ff7bcbb435c2bf05243e33b1c3d038a
snesjhon <snesjhon@gmail.com>
2019-09-24 00:06:52 -0700

    [INIT] Add resume

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Box } from "rebass";
 import Head from "./Head";
+import styled from "styled-components";
 import "../../node_modules/github-markdown-css/github-markdown.css";
+
+const MainLayoutWrapper = styled(Box)`
+  @media print {
+    @page {
+      margin: 0.5rem;
+    }
+    border: transparent !important;
+    .breadcrumbs {
+      display: none;
+    }
+  }
+`;
 
 const MainLayout = ({ children }) => (
   <>
     <Head />
-    <Box
+    <MainLayoutWrapper
       className="markdown-body"
       maxWidth={["", "80vw", "70vw", "60vw", "50vw"]}
       mx="auto"
@@ -14,7 +27,7 @@ const MainLayout = ({ children }) => (
       sx={{ border: "1px solid #d1d5da", borderRadius: "3px" }}
     >
       {children}
-    </Box>
+    </MainLayoutWrapper>
   </>
 );
 
