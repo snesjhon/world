a4c677d8dd2b78b6e782c3ef565b8409d33df0aa
snesjhon <snesjhon@gmail.com>
2023-02-27 23:15:51 -0800

    feat: add og img

---
+++ src/pages/og/[...routes].ts
@@ -0,0 +1,12 @@
+import { OGImageRoute } from "astro-og-canvas";
+import { getCollection } from "astro:content";
+
+export const { getStaticPaths, get } = OGImageRoute({
+  param: "routes",
+  pages: await getCollection("blog"),
+  getSlug: (_, page) => page.id.replace(/\.(md|mdx)$/, ".png"),
+  getImageOptions: (_, page) => ({
+    title: page.data.title,
+    description: page.data.description,
+  }),
+});
