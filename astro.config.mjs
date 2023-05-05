160d4b66e8e5508ac5c0011fd4e231f43faa0fa4
snesjhon <snesjhon@gmail.com>
2023-05-05 15:39:20 -0700

    feat: add vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://snesjhon.com",
   integrations: [react(), mdx(), sitemap()],
@@ -11,4 +13,8 @@ export default defineConfig({
       noExternal: ["react-icons"],
     },
   },
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
