e5d78e771e0ec97ae237094036b4722943bc6235
Jhonatan Salazar <snesjhon@gmail.com>
2025-11-18 00:21:46 -0800

    update with resume
    Committer: Jhonatan Salazar <snesjhon@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,32 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import pdf from "astro-pdf";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://snesjhon.com",
-  integrations: [react(), mdx(), sitemap()],
+  integrations: [
+    react(),
+    mdx(),
+    sitemap(),
+    pdf({
+      baseOptions: {
+        pdf: {
+          scale: 0.93,
+          margin: {
+            top: "0.1in",
+            bottom: "0.1in",
+            left: "0.2in",
+            right: "0.2in",
+          },
+        },
+      },
+      pages: {
+        "/resume": true,
+      },
+    }),
+  ],
   vite: {
     ssr: {
       noExternal: ["react-icons"],
